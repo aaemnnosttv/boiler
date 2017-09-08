3b98c5d94a7f70b8a38d7ed2cb77d2d1edeff0c9
Evan Mattson <me@aaemnnost.tv>
2017-09-08 10:18:19 +0300

    add phpcs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ define('WP_TESTS_DIR', getenv('WP_TESTS_DIR'));
 
 require WP_TESTS_DIR . '/includes/functions.php';
 
-tests_add_filter('muplugins_loaded', function() {
+tests_add_filter('muplugins_loaded', function () {
     require_once BOILER__ENTRYPOINT;
 });
 
--- tests/feature/ItWorks.php
+++ tests/feature/ItWorks.php
@@ -5,7 +5,8 @@ namespace Tests;
 class ItWorksTest extends \WP_UnitTestCase
 {
     /** @test */
-    function it_tests_something_high_level_yet_meaningful() {
+    function it_tests_something_high_level_yet_meaningful()
+    {
         $this->assertTrue(true);
     }
 }
